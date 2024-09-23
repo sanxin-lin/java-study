034749d699350b4e9818b82affb627b5b856057b
sanxin-lin <857287645@qq.com>
2024-09-23 22:48:11 +0800

    feat: 登录接口

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/controller/LoginController.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/controller/LoginController.java
@@ -7,20 +7,29 @@
 package org.jeecg.modules.system.controller;
 
 import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.IdWorker;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import net.sf.json.JSONObject;
 import org.jeecg.common.api.vo.Result;
 import org.jeecg.common.constant.CommonConstant;
+import org.jeecg.common.util.PasswordUtil;
 import org.jeecg.common.util.oConvertUtils;
 import org.jeecg.common.util.Md5Util;
 import org.jeecg.common.util.RedisUtil;
 import org.jeecg.config.JeecgBaseConfig;
+import org.jeecg.modules.system.entity.SysUser;
+import org.jeecg.modules.system.model.SysLoginModel;
+import org.jeecg.modules.system.service.ISysUserService;
 import org.jeecg.modules.system.util.RandImageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,12 +42,66 @@ public class LoginController {
 
     @Autowired
     private RedisUtil redisUtil;
+    @Autowired
+    private ISysUserService sysUserService;
 
     private final String BASE_CHECK_CODES = "qwertyuiplkjhgfdsazxcvbnmQWERTYUPLKJHGFDSAZXCVBNM1234567890";
     private final JeecgBaseConfig jeecgBaseConfig;
 
     public LoginController(JeecgBaseConfig jeecgBaseConfig) {
         this.jeecgBaseConfig = jeecgBaseConfig;
+    }
+
+    @ApiOperation("登录接口")
+    @PostMapping("/login")
+    public Result<JSONObject> login(@RequestBody SysLoginModel sysLoginModel, HttpServletRequest request) {
+        Result<JSONObject> result = new Result<JSONObject>();
+        String username = sysLoginModel.getUsername();
+        String password = sysLoginModel.getPassword();
+        if(isLoginFailOvertimes(username)) {
+            return result.error500("该用户登录失败次数过多，请于10分钟后再次登录！");
+        }
+
+        // step.1 验证码check
+        String captcha = sysLoginModel.getCaptcha();
+        if (captcha == null) {
+            return result.error500("验证码无效");
+        }
+
+        String lowerCaseCaptcha = captcha.toLowerCase();
+        String origin = lowerCaseCaptcha + sysLoginModel.getCheckKey() + jeecgBaseConfig.getSignatureSecret();
+        String realKey = Md5Util.md5Encode(origin, "utf-8");
+        Object checkCode = redisUtil.get(realKey);
+        if (checkCode == null || !checkCode.toString().equals(lowerCaseCaptcha)) {
+            log.warn("验证码错误，key= {} , Ui checkCode= {}, Redis checkCode = {}", sysLoginModel.getCheckKey(), lowerCaseCaptcha, checkCode);
+            result.error500("验证码错误");
+            // 改成特殊的code 便于前端判断
+            result.setCode(HttpStatus.PRECONDITION_FAILED.value());
+            return result;
+        }
+
+        // step.2 校验用户是否存在且有效
+        LambdaQueryWrapper<SysUser> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(SysUser::getUsername, username);
+        SysUser sysUser = sysUserService.getOne(queryWrapper);
+        result = sysUserService.checkUserIsEffective(sysUser);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        // step.3 校验用户名或密码是否正确
+        String userPassword = PasswordUtil.encrypt(username, password, sysUser.getSalt());
+        String sysPassword = sysUser.getPassword();
+        if (!sysPassword.equals(userPassword)) {
+            addLoginFailOvertimes(username);
+            result.error500("用户名或密码错误");
+            return result;
+        }
+
+        // step.4  登录成功获取用户信息
+        //        userIn
+
+        return result;
     }
 
     /**
@@ -75,6 +138,29 @@ public class LoginController {
     }
 
     /**
+     * 获取用户扫码后保存的token
+     */
+    @ApiOperation(value = "获取用户扫码后保存的token", notes = "获取用户扫码后保存的token")
+    @GetMapping("/getQrcodeToken")
+    public Result<?> getQrcodeToken(@RequestParam String qrcodeId) {
+        Object token = redisUtil.get(CommonConstant.LOGIN_QRCODE_TOKEN + qrcodeId);
+        Map result = new HashMap<>(5);
+        Object qrcodeIdExpire = redisUtil.get(CommonConstant.LOGIN_QRCODE + qrcodeId);
+        if (oConvertUtils.isEmpty(qrcodeIdExpire)) {
+            //二维码过期通知前台刷新
+            result.put("token", "-2");
+            return Result.ok(result);
+        }
+        if (oConvertUtils.isNotEmpty(token)) {
+            result.put("token", token);
+            result.put("success", true);
+        } else {
+            result.put("token", "-1");
+        }
+        return Result.ok(result);
+    }
+
+    /**
      * 后台生成图形验证码 ：有效
      * @param response
      * @param key
@@ -104,5 +190,30 @@ public class LoginController {
             res.error500("获取验证码失败,请检查redis配置!");
         }
         return res;
+    }
+
+    /**
+     * 记录登录失败次数
+     * @param username
+     */
+    public void addLoginFailOvertimes(String username) {
+        String key = CommonConstant.LOGIN_FAIL + username;
+        Object failTime = redisUtil.get(key);
+        Integer val = 0;
+        if (failTime != null) {
+            val = Integer.parseInt(failTime.toString());
+        }
+        // 10分钟，一分钟为60s
+        redisUtil.set(key, ++val, 600);
+    }
+
+    /**
+     * 登录失败超出次数5 返回true
+     * @param username
+     * @return
+     */
+    public boolean isLoginFailOvertimes(String username) {
+        Object failTime = redisUtil.get(CommonConstant.LOGIN_FAIL + username);
+        return failTime != null && Integer.parseInt(failTime.toString()) >= 5;
     }
 }
